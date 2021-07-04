82131eae8734651142458c7b5444d421264b2929
Matias Repetto Zecchi <60527450+MatiasRepetto@users.noreply.github.com>
2021-07-03 22:23:40 -0300

    Update exm20121feb-HashStock.cpp

--- p2/exm20121feb-HashStock.cpp
+++ p2/exm20121feb-HashStock.cpp
@@ -18,12 +18,40 @@ typedef _rep_hash * Stock;
 
 bool estaprod(char *prod, Stock &s){
     int direccion = h(prod) % s->tope;
-    while(s->ha[]){
-        
+    nodohash *aux = s->arrayhash[h(prod)% s->tope];
+    while(aux != NULL && !strcmp(aux->product, prod)){
+        aux = aux->sig;
     }
+    return aux!=NULL;
 }
 
 void ingresarProducto(char *prod, int cant, Stock &s){
     int direccion = h(prod) % s->tope;
-    
+    if(!estaprod(prod, s)){
+        nodohash *aux = new nodohash
+        aux->product = product;
+        aux->unidades = cant;
+        aux = s->arrayhash[direccion];
+        s->arrayhash[direccion] = aux;
+        s->cantelems++;
+    }else{
+        nodohash aux = s->arrayhash[direccion];
+        while(aux != NULL && !strcmp(aux->product, prod)){
+            aux->sig;
+        }
+        aux->unidades++;
+    }   
+}
+
+int cantidaddeProduct(char *prod, Stock &s){
+    if(!estaprod(prod, s)){
+        return 0;
+    }else{
+        int direccion = h(prod) % s->tope;
+        nodohash aux = s->arrayhash[direccion];
+        while(aux != NULL && !strcmp(aux->product, prod)){
+            aux->sig;
+        }
+        return aux->unidades;
+    }
 }
